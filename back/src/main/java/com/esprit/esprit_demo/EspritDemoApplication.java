a3b29d2201e7224208d07c9edc78cd9b0fbf2fdd
Houssem Hannachi <houssem.hannachi@euranova.eu>
2024-12-14 01:05:23 +0100

    first commit

---
+++ back/src/main/java/com/esprit/esprit_demo/EspritDemoApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.esprit_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspritDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspritDemoApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/esprit/esprit_demo/EspritDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.esprit_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspritDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
