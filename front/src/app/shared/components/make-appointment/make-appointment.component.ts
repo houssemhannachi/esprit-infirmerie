e000eb3a09951e1ac0a1797f202afd536b7c6ccc
Houssem Hannachi <houssem.hannachi@euranova.eu>
2024-12-24 17:03:40 +0100

    add form of make appointment component

--- front/src/app/shared/components/make-appointment/make-appointment.component.ts
+++ front/src/app/shared/components/make-appointment/make-appointment.component.ts
@@ -2,13 +2,28 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
 import {FooterComponent} from '../footer/footer.component';
 import {UserComponent} from '../user/user.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-make-appointment',
-  imports: [HeaderComponent, FooterComponent, UserComponent],
+  imports: [HeaderComponent, FooterComponent, UserComponent,
+    FormsModule
+  ],
   templateUrl: './make-appointment.component.html',
   styleUrl: './make-appointment.component.css'
 })
 export class MakeAppointmentComponent {
+  appointment: any = {
+    classe: '',
+    type: '',
+    date: '',
+    notes: ''
+  };
+
+  submitAppointment() {
+    console.log('Appointment Submitted:', this.appointment);
+    // You can handle further logic here, such as sending the data to a backend.
+  }
 
 }
