c31a68e6582b08970892c6b59eb2790f63ee35e3
Aymen LAGHA <aymenlagha997@gmail.com>
2024-12-27 23:48:57 +0100

    Fix left bar visibility

--- front/src/app/shared/components/left-side-bar/left-side-bar.component.ts
+++ front/src/app/shared/components/left-side-bar/left-side-bar.component.ts
@@ -1,17 +1,21 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {MakeAppointmentComponent} from '../make-appointment/make-appointment.component';
 import {Router} from '@angular/router';
 import {AuthService} from '../../auth/auth.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-left-side-bar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './left-side-bar.component.html',
   styleUrl: './left-side-bar.component.css'
 })
 export class LeftSideBarComponent {
+  userRole: string;
   username: any;
 
   constructor(private router: Router, private authService: AuthService) {
+    this.userRole = authService.getUserRole();
     this.username = this.authService.getLoggedUser().username;
   }
 
