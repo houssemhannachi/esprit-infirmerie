55f01df294defeb650effe42fc106d6d4e9c5bbc
Houssem Hannachi <houssem.hannachi@euranova.eu>
2024-12-28 03:20:52 +0100

    fix register form

--- front/src/app/shared/components/register/register.component.ts
+++ front/src/app/shared/components/register/register.component.ts
@@ -5,19 +5,26 @@ import {FooterComponent} from '../footer/footer.component';
 import {WelcomeComponent} from '../welcome/welcome.component';
 import {UserService} from '../../../core/services/user.service';
 import {ActivatedRoute, Router, RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
-  imports: [FormsModule, HeaderComponent, FooterComponent, WelcomeComponent, RouterModule, ReactiveFormsModule]
+  imports: [FormsModule,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    RouterModule,
+    ReactiveFormsModule]
 })
 export class RegisterComponent {
   constructor(private fb: FormBuilder,
               private userService: UserService,
               private router: Router,
               private route: ActivatedRoute) {
-    this.registerFrom = this.fb.group({
+    this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       lastName: ['', Validators.required],
       firstName: ['', Validators.required],
@@ -27,12 +34,12 @@ export class RegisterComponent {
     });
   }
 
-  registerFrom: FormGroup;
+  registerForm: FormGroup;
 
   onSubmit() {
-    console.log(this.registerFrom.value);
+    console.log(this.registerForm.value);
 
-    this.userService.createUser(this.registerFrom.value).pipe().subscribe({
+    this.userService.createUser(this.registerForm.value).pipe().subscribe({
       next: () => {
         this.router.navigate(['']);
       },
